c48fe1b6386ee54939ae936c8d3d2a748bcf3903
tsvetinkv <cnikolova95@gmail.com>
2023-12-29 23:06:38 +0200

    added eclipses page

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,19 @@
 import Inspect from "vite-plugin-inspect";
-import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [Inspect()],
-  root: "./",
-  base: "./",
-  build: {
-    outDir: "dist",
-    rollupOptions: {
-      input: {
-        main: "./index.html",
-        formation: "./formation/index.html",
-        moonPhases: "./moonPhases/index.html"
-        // eclipses: resolve(__dirname, 'eclipses/index.html'),
-      },
+    plugins: [Inspect()],
+    root: "./",
+    base: "./",
+    build: {
+        outDir: "dist",
+        rollupOptions: {
+            input: {
+                main: "./index.html",
+                formation: "./formation/index.html",
+                moonPhases: "./moonPhases/index.html",
+                eclipses: "./eclipses/index.html",
+            },
+        },
     },
-  },
 });
